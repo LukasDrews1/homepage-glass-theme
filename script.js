80a26fcb5ecb4a11d6d676ec2e67b2035df95c06
LukasDrews1 <84077918+LukasDrews1@users.noreply.github.com>
2022-10-25 17:25:52 +0200

    create div tag

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 const profilePicture = document.getElementById("profile-pic");
 const realFile = document.getElementById("real-file");
 const customButton = document.getElementById("custom-button");
+const imageElement = document.createElement("div");
+
+customButton.addEventListener("click", function(){
+    realFile.click();
+});
+
