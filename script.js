e3c2f64cf29ef3a9568520dfa3541f7001ef85d7
LukasDrews1 <84077918+LukasDrews1@users.noreply.github.com>
2022-10-25 14:29:04 +0200

    Add a const for custom-button

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
 const profilePicture = document.getElementById("profile-pic");
 const realFile = document.getElementById("real-file");
+const customButton = document.getElementById("custom-button");
