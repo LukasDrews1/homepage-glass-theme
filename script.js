ecdf57aab1226a3f62dc63fe78f5e7c9ec6a6a8e
LukasDrews1 <84077918+LukasDrews1@users.noreply.github.com>
2022-10-26 11:32:29 +0200

    Add a function to create a Element with class

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 const profilePicture = document.getElementById("profile-pic");
 const realFile = document.getElementById("real-file");
 const customButton = document.getElementById("custom-button");
-const imageElement = document.createElement("div");
+const pictureParent = document.getElementById("picture-parent");
 
 customButton.addEventListener("click", function(){
     realFile.click();
+    createImages()
 });
 
+function createImages() {
+    const imageElement = document.createElement("div");
+    pictureParent.append(imageElement);
+    imageElement.classList.add("images");
+}
